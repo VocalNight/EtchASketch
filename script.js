e6d9cf4c46b32c04671e0e3fa67fa4d9f22507ed
Vocal <vocalnight@protonmail.com>
2023-01-22 20:34:17 -0300

    added button to reset grid

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+let numGrid = 16;
+
 const container = document.querySelector('.container');
 
+const layoutButton = document.querySelector('.layoutSize');
 
-for (let i = 0; i < 16 * 16; i++) {
-    const gridElement = document.createElement('div')
-    gridElement.classList.add('test');
-    gridElement.addEventListener('mouseover', changeColor)
-    container.appendChild(gridElement);
+layoutButton.addEventListener('click', () => {
+    let size = prompt('Type the size of the grid here')
+    generateGrid(size);
+})
+
+function generateGrid(num) {
+
+    container.innerHTML = '';
+    for (let i = 0; i < num * num; i++) {
+        const gridElement = document.createElement('div')
+        gridElement.classList.add('test');
+        gridElement.addEventListener('mouseover', changeColor)
+        container.appendChild(gridElement);
+    }
 }
-
 
 function changeColor(event) {
     event.target.setAttribute('style', 'color: blue; background: grey');
 }
+
+generateGrid(numGrid);
